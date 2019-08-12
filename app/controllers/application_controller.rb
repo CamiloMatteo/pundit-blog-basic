88c57f26ab8c0fbde22e891044a9fd86c966b966
kalmes <matteo.camilo.k@gmail.com>
2019-08-12 12:12:02 -0400

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  include Pundit
+
+  private
+
+  def user_not_authorized
+    flash['warning'] = 'You are not authorized'
+    redirect_to request.referrer || root_path
+  end
+end
